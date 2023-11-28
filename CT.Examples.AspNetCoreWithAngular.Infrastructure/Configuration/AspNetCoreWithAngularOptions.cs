41d1821a24d8589e4cbc0744984e6bd9b0c46b35
Camilo Terevinto <camiloterevinto@outlook.com>
2023-11-28 07:40:52 +0000

    Updates to .NET 8 and Angular 17 (#7)
    * Update .NET version to 8
    
    * Revert upgrade to Angular 16 as it broke the build
    
    * Update angular core to 16
    
    * Update Angular Material to 16
    
    * Update ngrx to v16
    
    * Update Angular core to v17
    
    * Update Angular Material to v17
    
    * Update ngrx to v17
    
    * Fix a moderate severity vulnerability
    
    * Update remaining nuget packages and fix some warnings

--- CT.Examples.AspNetCoreWithAngular.Infrastructure/Configuration/AspNetCoreWithAngularOptions.cs
+++ CT.Examples.AspNetCoreWithAngular.Infrastructure/Configuration/AspNetCoreWithAngularOptions.cs
@@ -1,11 +1,11 @@
 ﻿namespace CT.Examples.AspNetCoreWithAngular.Infrastructure.Configuration;
 
 internal class AspNetCoreWithAngularOptions
-{
-    public string? Server { get; set; }
-    public string? Database { get; set; }
-    public string? Username { get; set; }
-    public string? Password { get; set; }
+{   
+    public string Server { get; set; }
+    public string Database { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
 
     public void Validate()
     {
